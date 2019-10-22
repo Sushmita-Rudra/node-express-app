45881d3aa88ee8641169b4bd84dbffd69fa47aef
Sushmmita-Rudra <S537491@NWMISSOURI.EDU>
2019-10-22 14:36:30 -0500

    made changes in app.js file

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const port = process.env.PORT || config.get("port");
 // Use Express app.get() methods to configure endpoints
 
 // declare your callback function the old way
+/**
 app.get('/', function (req, res) {
   res.send('Welcome to the default page!  <br> <br>' +
     'Try going to different URIs by adding these at the end: <br> <br>' +
@@ -31,34 +32,43 @@ app.get('/', function (req, res) {
     'Fork the source code from <a href="https://github.com/denisecase/node-express-app">https://github.com/denisecase/node-express-app</a>'
   )
 })
+*/
+
+app.get('/', function (req, res) {
+    res.send('Welcome to my Module 4 assignment  on creating Express App<br> ' +
+    'This reponse is triggered when the use enters "/" in the URL'
+    )
+  })
+
+
 
 // or use the new arrow function syntax
 // respond with text
 app.get('/hello', (req, res) => {
-  res.send('Hello World!')
+  res.send('hello Maryville!')
 })
 
 // or respond with html
 app.get('/big', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>HELLO WORLD!</h1>')
 })
 
 // or respond with JSON
 app.get('/json', (req, res) => {
-  res.send('{"name" : "Nandini"}')
+  res.send('{"name" : "Rudra Sushmita"}')
 })
 
 // :name indicates a parameter at this location in the URI
 app.get('/greeting/:id', (req, res) => {
-  res.send(`Hello! The id provided was ${req.params.id}.`)
+  res.send(`Hey! The id provided was ${req.params.id}.`)
 })
 
 // combine your skills and get creative
 app.get('/yo/:buddy', (req, res) => {
-  res.send(`<h1>Yo, ${req.params.buddy}!</h1>`)
+  res.send(`<h1>Yo what's up !, ${req.params.buddy}!</h1>`)
 })
-
-// provide multiple query parameters (named first and last) with ? and &
+/**
+ // provide multiple query parameters (named first and last) with ? and &
 app.get('/fancy', (req, res) => {
   const first = req.query.first
   const last = req.query.last
@@ -72,7 +82,7 @@ let fortunes = ['It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Ye
 'Very doubtful.']
 
 // Implements a Magic 8 Ball service
-app.get('/fortune', (req,res) => {
+app.get('/joke', (req,res) => {
   if(isEmpty(req.query)){
     res.send('<h2>You wish to know the future?</h2>' +
              '<p>Ask a question in the query string, e.g., http://localhost:3002/fortune?Will I become rich? <br/>' +
@@ -81,6 +91,28 @@ app.get('/fortune', (req,res) => {
     res.send(`The answer is ... wait for it ... ${fortunes[randomInt(0, fortunes.length)]}`)
   }
 })
+*/
+app.get('/fancy', (req, res) => {
+  const first = req.query.first
+  const last = req.query.last
+  res.send(`Hello ${first} ${last}!`)
+})
+
+let jokes = ['Chuck Norris sheds his skin twice a year.', 'Chuck Norris doesnt shower, he only takes blood baths.', 'Chuck Norris has two speeds: Walk and Kill.', 'When Chuck Norris talks, everybody listens. And dies.',
+'Chuck Norris doesnt wash his clothes. He disembowels them.', 'Chuck Norris can win a game of Connect Four in only three move', 'Chuck Norris can divide by zero.', 'Chuck Norris always knows the EXACT location of Carmen SanDiego..', 'Chuck Norris has 12 moons. One of those moons is the Earth.', 'It takes Chuck Norris 20 minutes to watch 60 Minutes.',
+]
+
+// Implements a Joke master service
+app.get('/joke', (req,res) => {
+  if(isEmpty(req.query)){
+    res.send('<h2>You wish to hear a joke?</h2>' +
+             '<p>Ask a question in the query string, e.g., http://localhost:3002/joke?Can I hear the lamest joke ever? <br/>' +
+             '<p>The Joke master will answer!</p>')
+  } else {
+    res.send(`The joke is ...  ${jokes[randomInt(0, jokes.length)]}`)
+  }
+})
+
 
 // Use middleware to handle all non-managed routes (e.g. /xyz)
 // https://expressjs.com/en/api.html#req.originalUrl
@@ -95,10 +127,10 @@ app.listen(port, hostname, () => {
   console.log(`   Try /hello`)
   console.log(`   Try /big`)
   console.log(`   Try /json`)
-  console.log(`   Try /fortune`)
+  console.log(`   Try /joke`)
   console.log(`   Try /greeting/yourname`)
   console.log(`   Try /yo/Dr.Rogers`)
-  console.log(`   Try /fancy/?first=Denise&last=Case`)
+  console.log(`   Try /fancy/?first=Sushmita&last=Rudra`)
   console.log('\n Hit CTRL-C CTRL-C to stop\n')
 })
 
